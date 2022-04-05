5f7da8b49b1466d4930e3bef0a4c11bb8d7a47fb
“MarcMD” <meierdom@my.erau.edu>
2022-04-04 20:04:43 -0400

    Added first model and scheduled prediction

---
+++ deployment/05_model/001_create_model.sh
@@ -0,0 +1,13 @@
+# Create model
+bq query \
+--use_legacy_sql=false \
+'CREATE OR REPLACE MODEL `crypto-sentiment-341504.crypto_sentiment.bitcoin_model`
+OPTIONS (model_type="linear_reg", input_label_cols=["next_period_price_movement"], data_split_method="random", data_split_eval_fraction=0.2) AS
+SELECT 
+bitcoin.avg_compound_sentiment, bitcoin.avg_neg_sentiment, bitcoin.avg_pos_sentiment, bitcoin.volume_traded, 
+bitcoin.tweets_count, bitcoin.trades_count, bitcoin.price_open,
+bitcoin.price_close, bitcoin.next_period_price_movement
+FROM
+`crypto-sentiment-341504.crypto_sentiment.bitcoin_sentiments_consolidated` as bitcoin
+WHERE 
+next_period_price_movement IS NOT NULL;'
